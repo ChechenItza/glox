54f02f4d840f2238d0cf4ef72ed8bd9b8fa0ecfe
Chechen Itza <artka.kazaktar@gmail.com>
2022-11-26 17:05:13 +0600

    simplify debug structure

--- interpreter/debug.go
+++ interpreter/debug.go
@@ -34,3 +34,28 @@ func DisassembleInstruction(c *Chunk, offset int) int {
 		return offset + 1
 	}
 }
+
+func simpleInstruction(name string, offset int) int {
+	fmt.Printf("%s\n", name)
+	return offset + 1
+}
+
+func constantInstruction(name string, c *Chunk, offset int) int {
+	i := c.Code[offset+1]
+
+	fmt.Printf("%-16s %4d '", name, i)
+	c.Constants[i].Print()
+	fmt.Printf("'\n")
+
+	return offset + 2
+}
+
+func longConstantInstruction(name string, c *Chunk, offset int) int {
+	i := ConcatLongConstant(c.Code[offset+1 : offset+4]...)
+
+	fmt.Printf("%-16s %4d '", name, i)
+	c.Constants[i].Print()
+	fmt.Printf("'\n")
+
+	return offset + 4
+}
